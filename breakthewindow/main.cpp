1f743d01466e28b5ce372ec6aca47f172b7bec6d
Prayag Upd <prayag.upd@gmail.com>
2016-04-06 11:25:19 -0400

    Initial Commit

---
+++ breakthewindow/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  breakthewindow
+//
+//  Created by Prayag Upd on 4/6/16.
+//  Copyright (c) 2016 pseudononymous. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

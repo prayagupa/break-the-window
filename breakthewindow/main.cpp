43c425bd7c7dc3221097238f22b0e31a80de235d
Prayag Upd <prayag.upd@gmail.com>
2016-04-07 00:47:03 -0400

    add simple c++ example

--- breakthewindow/main.cpp
+++ breakthewindow/main.cpp
@@ -7,9 +7,10 @@
 //
 
 #include <iostream>
+#include <tesseract/baseapi.h>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "break the window";
     return 0;
 }
